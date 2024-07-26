fd5562ac19bb415f75a6be3df8c55b87dc5c4220
Kalil Pereira <kalil.pereira@fatec.sp.gov.br>
2024-07-26 17:47:48 -0300

    Adicionando mais projetos

--- src/data/projetos.ts
+++ src/data/projetos.ts
@@ -9,5 +9,21 @@ export const projetos: Projeto[] = [
         titulo: "AReS",
         foto: "images/AReS.png",
         texto: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error enim eaque magni voluptas numquam! Vitae aliquid minus, cumque veritatis, et facilis in beatae velit repudiandae aliquam dolorum ab repellat assumenda voluptatum illum itaque ipsam. Eveniet autem voluptate quod, fugit voluptatum sequi esse voluptatibus! Fugit hic laborum velit aspernatur beatae, vel vitae. Maiores esse porro suscipit molestias delectus ipsam aliquam natus voluptas necessitatibus, eveniet repellendus soluta earum quam ipsa dolorum! Ea reiciendis, dolores accusantium ad vel nisi iusto corporis aut laudantium. Dolores unde ad nobis itaque et? Totam quas debitis assumenda, suscipit deleniti, impedit sit voluptatibus quaerat optio sint eligendi possimus tempore excepturi numquam quibusdam porro commodi fugit amet quisquam libero magni eum. Perferendis eum aperiam minus consectetur, magni at possimus."
+    },
+    {
+        titulo: "Lotus Chatbot",
+        foto: "images/Lotus.png",
+        texto: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error enim eaque magni voluptas numquam! Vitae aliquid minus, cumque veritatis, et facilis in beatae velit repudiandae aliquam dolorum ab repellat assumenda voluptatum illum itaque ipsam. Eveniet autem voluptate quod, fugit voluptatum sequi esse voluptatibus! Fugit hic laborum velit aspernatur beatae, vel vitae. Maiores esse porro suscipit molestias delectus ipsam aliquam natus voluptas necessitatibus, eveniet repellendus soluta earum quam ipsa dolorum! Ea reiciendis, dolores accusantium ad vel nisi iusto corporis aut laudantium. Dolores unde ad nobis itaque et? Totam quas debitis assumenda, suscipit deleniti, impedit sit voluptatibus quaerat optio sint eligendi possimus tempore excepturi numquam quibusdam porro commodi fugit amet quisquam libero magni eum. Perferendis eum aperiam minus consectetur, magni at possimus."
+    },
+    {
+        titulo: "Lotus Dashboard",
+        foto: "images/Dashboard.png",
+        texto: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error enim eaque magni voluptas numquam! Vitae aliquid minus, cumque veritatis, et facilis in beatae velit repudiandae aliquam dolorum ab repellat assumenda voluptatum illum itaque ipsam. Eveniet autem voluptate quod, fugit voluptatum sequi esse voluptatibus! Fugit hic laborum velit aspernatur beatae, vel vitae. Maiores esse porro suscipit molestias delectus ipsam aliquam natus voluptas necessitatibus, eveniet repellendus soluta earum quam ipsa dolorum! Ea reiciendis, dolores accusantium ad vel nisi iusto corporis aut laudantium. Dolores unde ad nobis itaque et? Totam quas debitis assumenda, suscipit deleniti, impedit sit voluptatibus quaerat optio sint eligendi possimus tempore excepturi numquam quibusdam porro commodi fugit amet quisquam libero magni eum. Perferendis eum aperiam minus consectetur, magni at possimus."
+    },
+    {
+        titulo: "CRUD IMC",
+        foto: "images/CRUDIMC.png",
+        texto: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error enim eaque magni voluptas numquam! Vitae aliquid minus, cumque veritatis, et facilis in beatae velit repudiandae aliquam dolorum ab repellat assumenda voluptatum illum itaque ipsam. Eveniet autem voluptate quod, fugit voluptatum sequi esse voluptatibus! Fugit hic laborum velit aspernatur beatae, vel vitae. Maiores esse porro suscipit molestias delectus ipsam aliquam natus voluptas necessitatibus, eveniet repellendus soluta earum quam ipsa dolorum! Ea reiciendis, dolores accusantium ad vel nisi iusto corporis aut laudantium. Dolores unde ad nobis itaque et? Totam quas debitis assumenda, suscipit deleniti, impedit sit voluptatibus quaerat optio sint eligendi possimus tempore excepturi numquam quibusdam porro commodi fugit amet quisquam libero magni eum. Perferendis eum aperiam minus consectetur, magni at possimus."
     }
+    
 ];
